287f8b9155964692d4cecc973d305a8fb126d2cf
Rafael Aguiar <rafael.a1@hotmail.com>
2024-12-07 11:54:42 -0300

    feat: ensure that image is loaded from backend

--- src/app/(singles)/(default)/single/[[...slug]]/page.tsx
+++ src/app/(singles)/(default)/single/[[...slug]]/page.tsx
@@ -1,16 +1,19 @@
+'use client';
 import React from "react";
 import NcImage from "@/components/NcImage/NcImage";
 import SingleHeader from "@/app/(singles)/SingleHeader";
+import { useGetArticleQuery } from "@/services/api/articles/ServiceArticles";
 
 const PageSingle = ({ params  }: any) => {
-  const slug = params.slug;
-  console.log(slug)
-  return (
+  const { data, isLoading } = useGetArticleQuery(params.slug)
+  const { featureImg } = data?.article || {};
+
+   return (
     <>
       <div className={`nc-PageSingle pt-8 lg:pt-16`}>
         <header className="container rounded-xl">
           <div className="max-w-screen-md mx-auto">
-            <SingleHeader />
+            <SingleHeader article={data}/>
           </div>
         </header>
 
@@ -19,7 +22,8 @@ const PageSingle = ({ params  }: any) => {
           alt="single"
           containerClassName="container my-10 sm:my-12"
           className="w-full rounded-xl"
-          src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg"
+          // src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg"
+          src={featureImg}
           width={1260}
           height={750}
           sizes="(max-width: 1024px) 100vw, 1280px"
